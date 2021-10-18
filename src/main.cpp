51b0a54b359e6a71bf39d1956221bdf1a9ad3c8a
Daniel-del-Castillo <alu0101225548@ull.edu.es>
2021-10-18 10:21:02 +0100

    Allow using a file for the strings

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdexcept>
 
 #include "headers/pda_reader.hpp"
 
 using std::ifstream;
 using std::cout;
-using std::string;
 using std::cin;
+using std::cerr;
+using std::string;
+using std::exception;
 
 struct Args {
     std::string definition_file;
@@ -37,7 +40,40 @@ void print_help() {
     cout << "It is also possible to specify the -d option which allows printing debug information\n";
 }
 
-int main(int argc, char** argv) {
+void enter_reading_loop(PDA* pda) {
+    string s;
+    while (true) {
+        getline(cin, s);
+        if (s == "exit") {
+            return;
+        }
+        if (pda->check_string(s)) {
+            cout << "The string " + s + " belongs to the language\n";
+        } else {
+            cout << "The string " + s + " doesn't belong to the language\n";
+        }
+    }
+}
+
+void evaluate_strings_from_file(PDA* pda, string filename) {
+    ifstream input_file(filename);
+    if (!input_file.is_open()) {
+        cerr << "Couldn't open the strings file\n";
+        return;
+    }
+    string s;
+    do {
+        getline(input_file, s);
+        if (pda->check_string(s)) {
+            cout << "The string " + s + " belongs to the language\n";
+        } else {
+            cout << "The string " + s + " doesn't belong to the language\n";
+        }
+    } while (!input_file.eof());
+    
+}
+
+int protected_main(int argc, char** argv) {
     if (argc < 2 || argc > 4) {
         print_help();
         return 0;
@@ -49,18 +85,25 @@ int main(int argc, char** argv) {
     }
 
     ifstream input_file(args.definition_file);
+    if (!input_file.is_open()) {
+        cerr << "Couldn't open the definition file\n";
+        return 1;
+    }
     PDA* pda = PDAReader::read_pda(input_file, args.debug);
-    string s;
-    while (true) {
-        getline(cin, s);
-        if (s == "exit") {
-            break;
-        }
-        if (pda->check_string(s)) {
-            cout << "The string " + s + " belongs to the language\n";
-        } else {
-            cout << "The string " + s + " doesn't belong to the language\n";
-        }
+    if (args.strings_file.empty()) {
+        enter_reading_loop(pda);
+    } else {
+        evaluate_strings_from_file(pda, args.strings_file);
     }
     free(pda);
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    try {
+        return protected_main(argc, argv);
+    } catch (const exception& e) {
+        cerr << "Something went wrong: " << e.what() << "\n";
+        return 1;
+    }
 }
